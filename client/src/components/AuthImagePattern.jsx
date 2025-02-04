d94446c9b390cda29959aae7b6a94cd8f2f53e0e
Ujjwal-1809 <ujjwalssharma12@gmail.com>
2025-02-04 21:14:40 +0530

    Updated favicon

--- client/src/components/AuthImagePattern.jsx
+++ client/src/components/AuthImagePattern.jsx
@@ -1,17 +1,10 @@
+import { MessagesSquare } from "lucide-react";
+
 const AuthImagePattern = ({ title, subtitle }) => {
     return (
-      <div className="hidden lg:flex items-center justify-center bg-base-200 p-12 h-screen">
-        <div className="max-w-md text-center">
-          <div className="grid grid-cols-3 gap-3 mb-8">
-            {[...Array(9)].map((_, i) => (
-              <div
-                key={i}
-                className={`aspect-square rounded-2xl bg-primary/10 ${
-                  i % 2 === 0 ? "animate-pulse" : ""
-                }`}
-              />
-            ))}
-          </div>
+      <div className="hidden lg:flex items-center justify-center bg-base-200 p-12 h-auto flex-col">
+        <div className="max-w-md text-center flex flex-col justify-center items-center gap-y-4">
+         <MessagesSquare size={144}/>
           <h2 className="text-2xl font-bold mb-4">{title}</h2>
           <p className="text-base-content/60">{subtitle}</p>
         </div>
